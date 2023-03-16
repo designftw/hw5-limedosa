3176f87ff1237133cdbd29fee0ff03bc6997cb60
limedosa <109569567+limedosa@users.noreply.github.com>
2023-03-15 23:15:24 -0400

    hi

---
+++ homepage/index.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var div = document.getElementById("myDiv");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
